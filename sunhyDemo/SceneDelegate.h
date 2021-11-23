a03bc5a3b8bda197775468daacbeaca5e2acc1d2
sunhy <1868297745@163.com>
2021-11-23 17:48:14 +0800

    1

---
+++ sunhyDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  sunhyDemo
+//
+//  Created by 研发部 on 2021/11/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ sunhyDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  sunhyDemo
+//
+//  Created by 研发部 on 2021/11/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
